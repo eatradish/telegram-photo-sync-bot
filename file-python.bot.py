b1feeaed69fd9b7b41e464a9cdd5c4415517fe6f
eatradish <qingbaodou@gmail.com>
2017-06-02 10:10:05 +0800

    Example

---
+++ file-python.bot.py
@@ -0,0 +1,21 @@
+import telegram
+import datetime
+import random
+
+bot = telegram.Bot(token='393669135:AAFvEkohrcs-X7yrfy0fzVZmZPCEV8hbqdA')
+
+def download():
+        #print([u.message.photo for u in updates if u.message.photo])
+    a = 0
+    while True:
+        updates = bot.get_updates()
+        u = updates[len(updates) - 1]
+        if u.message.photo:
+            if u != a:
+                photo_file = bot.get_file(u.message.photo[-1].file_id)
+                photo_file.download(custom_path='/var/www/file/ACG-img/' + 'photo-' + repr(datetime.datetime.now().timestamp()) + repr(random.random()*10) + '.jpg')
+                a = u
+            elif u == a:
+                continue
+        print(a)
+download()
